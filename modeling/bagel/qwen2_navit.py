f1eabb8f114e677c1d7af3010920820853ad8d3b
sko00o <lwylwy@outlook.com>
2025-06-17 23:18:30 +0800

    fix: relative import

--- modeling/bagel/qwen2_navit.py
+++ modeling/bagel/qwen2_navit.py
@@ -22,7 +22,7 @@ from torch.nn.functional import scaled_dot_product_attention
 from transformers.utils import ModelOutput
 
 from flash_attn import flash_attn_varlen_func
-from .modeling.qwen2.modeling_qwen2 import (
+from ...modeling.qwen2.modeling_qwen2 import (
     Qwen2Attention, 
     Qwen2MLP, 
     Qwen2PreTrainedModel, 
@@ -31,7 +31,7 @@ from .modeling.qwen2.modeling_qwen2 import (
     apply_rotary_pos_emb,
 )
 
-from .modeling.qwen2.configuration_qwen2 import Qwen2Config as _Qwen2Config
+from ...modeling.qwen2.configuration_qwen2 import Qwen2Config as _Qwen2Config
 
 
 torch._dynamo.config.cache_size_limit = 512
